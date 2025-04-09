cbd79f19ce8499ea4786a88c485268631ef4b1ea
WayneCommand <wayne@waynecommand.com>
2025-04-09 11:30:33 +0800

    update style.

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div>
           <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
             <Text size="xl" span>
-              💨
+              🌥
             </Text>
             <Text
               size="xl"
